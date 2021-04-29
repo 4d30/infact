997f6d83bc4569d2294dc613147c2253bc7c776e
Joe Mifsud <45021583+4d30@users.noreply.github.com>
2021-04-29 20:24:37 +0000

    Refactoring HTML parser to use Regex&JSON

--- infact.py
+++ infact.py
@@ -6,9 +6,14 @@ import time
 import requests
 import brotli
 from bs4 import BeautifulSoup
+import re
+import json
 import pickle
 import pandas as pd
+import psycopg2
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
 def add_to_mapping(DICT, KEY, VALUE ):
 	try:
@@ -32,8 +37,6 @@ if os.path.isfile(jobmap_cache):
 	print(f'Found {jobmap_cache}')
 	with open(jobmap_cache,'rb') as f:
 		jobmap = pickle.load(f)
-else:
-	jobmap = pd.DataFrame()
 
 headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
@@ -41,10 +44,25 @@ headers = {
 	'Accept-Encoding': 'gzip, deflate, br',
 	'Accept-Language': 'en-US,en;q=0.5'}
 
-
 SEARCH_URL= 'https://www.indeed.com/jobs?q=data+scientist&l=new+york,+ny&sort=date&start='
 script = ''
 print('Go!')
+
+def extract_job_dict(LINE):
+	line = LINE
+	tmp = pd.DataFrame()
+	if line.startswith('jobmap'):
+		line = re.sub('^.*= ','',line)
+		line = re.sub(r'(\w+):','"\\1":',line)
+		line = re.sub(r"'",'"',line)
+		line = re.sub(';$','',line)
+		try:
+			return tmp.append(json.loads(line),ignore_index=True)
+		except:
+			print(LINE)
+			print(line)
+	else:
+		return None
 for decade in range(0,100,10):
 	time.sleep(2)
 	print(decade)
@@ -53,45 +71,38 @@ for decade in range(0,100,10):
 	for js in soup.find_all('script'):
 		if 'jobmap' in str(js.contents):
 			script = js.contents[0]
-	tmp_map = dict()
+	df_tmp = pd.DataFrame()
 	for line in script.splitlines():
-		if 'jobmap' in line:
-			pairs = get_pairs(line)
-			if len(pairs) > 1:
-				for pair in pairs:
-					if ':' in pair:
-						kv_pair = pair.split(':')
-						add_to_mapping(tmp_map,
-								kv_pair[0],
-								kv_pair[1].strip("'"))
-	df_tmp = pd.DataFrame(tmp_map)
+		df_tmp = df_tmp.append(extract_job_dict(line))
+	df_tmp.set_index('jk',inplace=True)
 	if not os.path.isfile(jobmap_cache):
 		jobmap = pd.DataFrame(columns=df_tmp.columns)
-	for i in range(0,len(df_tmp)):
-		if df_tmp.iloc[i,0] not in jobmap.jk.values:
+	for jk in df_tmp.index.values:
+		if jk not in jobmap.jk.values:
 			print(True)
-			jobmap = jobmap.append(df_tmp.iloc[i,:])
+			jobmap.append(df_tmp.loc[jk])
 		else:
 			continue
-DESC_URL = 'https://www.indeed.com/viewjob?jk='
-for jk in jobmap['jk']:
-	filename = './jobs/'+jk
-	if os.path.isfile(filename):
-		continue
-	else:
-		print(jk)
-		URL = DESC_URL + jk
-		response = requests.get(URL, headers = headers )
-		soup = BeautifulSoup(response.text,'html.parser')
-		text = soup.get_text()
-		try:
-			text = text.split('Full Job Description')[1]
-			text = text.split('Report jobApply')[0]
-		except:
-			None
-		with open(filename,'w') as f:
-			f.write(text)
 
-jobmap['mtime'] = list(map(lambda x: os.path.getmtime(f'./jobs/{x}'),jobmap['jk']))
-with open(jobmap_cache,'wb') as f:
-	pickle.dump(jobmap,f)
+#DESC_URL = 'https://www.indeed.com/viewjob?jk='
+#for jk in jobmap['jk']:
+#	filename = './jobs/'+jk
+#	if os.path.isfile(filename):
+#		continue
+#	else:
+#		print(jk)
+#		URL = DESC_URL + jk
+#		response = requests.get(URL, headers = headers )
+#		soup = BeautifulSoup(response.text,'html.parser')
+#		text = soup.get_text()
+#		try:
+#			text = text.split('Full Job Description')[1]
+#			text = text.split('Report jobApply')[0]
+#		except:
+#			None
+#		with open(filename,'w') as f:
+#			f.write(text)
+#
+#jobmap['mtime'] = list(map(lambda x: os.path.getmtime(f'./jobs/{x}'),jobmap['jk']))
+#with open(jobmap_cache,'wb') as f:
+#	pickle.dump(jobmap,f)
