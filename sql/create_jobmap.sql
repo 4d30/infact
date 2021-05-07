65d76961415c2dc10a2cce6929c868ee7ca0d1fd
Joe Mifsud <45021583+4d30@users.noreply.github.com>
2021-05-07 19:28:49 +0000

    Migrated cache to RDB

--- sql/create_jobmap.sql
+++ sql/create_jobmap.sql
@@ -1,10 +1,10 @@
-DROP TABLE jobmap;
+/*DROP TABLE jobmap;*/
 CREATE TABLE jobmap(
 	jk char(16) PRIMARY KEY,
 	efccid char(16),
 	srcid char(16),
 	cmpid char(16),
-	srcname varchar(50),
+	srcname varchar(100),
 	cmp varchar(75),
 	cmpesc varchar(75),
 	cmplnk varchar(100),
@@ -12,8 +12,6 @@ CREATE TABLE jobmap(
 	country varchar(20),
 	zip char(5),
 	city char(16),
-	title varchar(100),
+	title varchar(150),
 	locid char(16),
 	rd varchar(64));
-	
-
---
+++ sql/create_pubdate.sql
@@ -0,0 +1,4 @@
+DROP TABLE pub_dates;
+CREATE TABLE pub_dates(
+	jk char(16) PRIMARY KEY,
+	pub_date date);
