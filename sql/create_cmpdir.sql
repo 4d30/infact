ee1e0ce89bdf556ab940398ae6baf6481791e7ef
Joe Mifsud <45021583+4d30@users.noreply.github.com>
2021-05-10 19:25:22 +0000

    Added table to host company information

---
+++ sql/create_cmpdir.sql
@@ -0,0 +1,10 @@
+DROP TABLE cmpdir;
+CREATE TABLE cmpdir(
+	cmp varchar(75) PRIMARY KEY,
+	headquarters varchar(300),
+	revenue varchar(50),
+	employees varchar(25),
+	industry varchar(50),
+	links varchar(150)
+	);
+
