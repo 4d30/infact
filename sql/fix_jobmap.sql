31d94a837bd2d41a4be130ed4ea9869e51c568d0
Joe Mifsud <45021583+4d30@users.noreply.github.com>
2021-05-17 17:50:51 +0000

    Fix some insane schema choices

---
+++ sql/fix_jobmap.sql
@@ -0,0 +1,17 @@
+ALTER TABLE jobmap ALTER COLUMN jk TYPE text;
+ALTER TABLE jobmap ALTER COLUMN efccid TYPE text;
+ALTER TABLE jobmap ALTER COLUMN srcid TYPE text;
+ALTER TABLE jobmap ALTER COLUMN cmpid TYPE text;
+ALTER TABLE jobmap ALTER COLUMN srcname TYPE text;
+ALTER TABLE jobmap ALTER COLUMN cmp TYPE text;
+ALTER TABLE jobmap ALTER COLUMN cmpesc TYPE text;
+ALTER TABLE jobmap ALTER COLUMN cmplnk TYPE text;
+ALTER TABLE jobmap ALTER COLUMN loc TYPE text;
+ALTER TABLE jobmap ALTER COLUMN country TYPE text;
+ALTER TABLE jobmap ALTER COLUMN zip TYPE text;
+ALTER TABLE jobmap ALTER COLUMN city TYPE text;
+ALTER TABLE jobmap ALTER COLUMN title TYPE text;
+ALTER TABLE jobmap ALTER COLUMN locid TYPE text;
+ALTER TABLE jobmap ALTER COLUMN rd TYPE text;
+ALTER TABLE pub_dates ALTER COLUMN jk TYPE text;
+ALTER TABLE leaderboard ALTER COLUMN term TYPE text;
