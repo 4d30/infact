c81a144501ea979b75bb9bcf70d8961c3e76dac1
Joe Mifsud <45021583+4d30@users.noreply.github.com>
2021-05-18 18:30:32 +0000

    Altered schema for more sanity. Changed char and varchar types to text

--- sql/create_cmpdir.sql
+++ sql/create_cmpdir.sql
@@ -1,10 +1,10 @@
 DROP TABLE cmpdir;
 CREATE TABLE cmpdir(
-	cmp varchar(75) PRIMARY KEY,
-	headquarters varchar(300),
-	revenue varchar(50),
-	employees varchar(25),
-	industry varchar(50),
-	links varchar(150)
+	cmp text PRIMARY KEY,
+	headquarters text,
+	revenue text,
+	employees text,
+	industry text,
+	links text
 	);
 
